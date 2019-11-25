6f7a431557280c702918895f8112407a34b39768
celine305 <celine.abessolo@icloud.com>
2019-11-25 16:14:58 +0000

    added playground folder to gitignore

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,36 +1,43 @@
 'use strict';
 
-var visible = false;
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-var showHide = function showHide() {
-  visible = !visible;
-  renderApp();
-};
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-var appRoot = document.getElementById('app');
+var Person = function () {
+  function Person() {
+    var name = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 'Anonymous';
+    var age = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 0;
 
-var renderApp = function renderApp() {
-  var template = React.createElement(
-    'div',
-    null,
-    React.createElement(
-      'h1',
-      null,
-      'Visibility Toggle'
-    ),
-    React.createElement(
-      'button',
-      { onClick: showHide },
-      visible ? 'Hide Details' : 'Show deatils'
-    ),
-    visible && React.createElement(
-      'p',
-      null,
-      'Hey. These are some details you can now see!'
-    )
-  );
+    _classCallCheck(this, Person);
 
-  ReactDOM.render(template, appRoot);
-};
+    this.name = name;
+    this.age = age;
+  }
 
-renderApp();
+  _createClass(Person, [{
+    key: 'getGreeting',
+    value: function getGreeting() {
+      return 'Hi. I am ' + this.name + '!';
+    }
+  }, {
+    key: 'getDescription',
+    value: function getDescription() {
+      return this.name + ' is ' + this.age + ' years old.';
+    }
+  }]);
+
+  return Person;
+}();
+
+;
+
+var me = new Person('Céline Abessolo', '28');
+console.log(me); //Person {name: "Céline Abessolo"}
+console.log(me.getGreeting()); //Hi. I am Céline Abessolo!
+console.log(me.getDescription()); //Céline Abessolo is 28 years old.
+
+var other = new Person();
+console.log(other); //Person {name: "Anonymous"}
+console.log(other.getGreeting()); //Hi. I am Anonymous!
+console.log(other.getDescription()); //Anonymous is 0 years old.
