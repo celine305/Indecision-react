efb564a26cdb59c1b71b9140e7856964c215a55d
celine305 <celine.abessolo@icloud.com>
2019-12-03 16:22:46 +0000

    convert header, option, and options to stateless functional components

--- src/app.js
+++ src/app.js
@@ -54,53 +54,45 @@ class IndecisionApp extends React.Component {
   }
 }
 
-class Header extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>{this.props.title}</h1>
-        <h2>{this.props.subtitle}</h2>
-      </div>
-    )
-  }
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.title}</h1>
+      <h2>{props.subtitle}</h2>
+    </div>
+  )
 }
 
-class Action extends React.Component {
-  render() {
-    return (
-      <div>
+const Action = (props) => {
+  return (
+    <div>
         <button
-          onClick={this.props.handlePick}
-          disabled={!this.props.hasOption}
+          onClick={props.handlePick}
+          disabled={!props.hasOption}
         >
           What should I do?
         </button>
       </div>
-    )
-  }
+  )
 }
 
-class Options extends React.Component {
-  render() {
-    return (
-      <div>
-        <button onClick={this.props.handleDeleteOptions}>Remove All</button>
-        {
-          this.props.options.map((option)=> <Option key={option} optionText={option}/>)
-        }
-      </div>
-    );
-  }
+const Options = (props) => {
+  return (
+    <div>
+      <button onClick={props.handleDeleteOptions}>Remove All</button>
+      {
+        props.options.map((option)=> <Option key={option} optionText={option}/>)
+      }`
+     </div>
+  )
 }
 
-class Option extends React.Component {
-  render() {
-    return (
-      <div>
-        {this.props.optionText}
-      </div>
-    )
-  }
+const Option = (props) => {
+  return (
+   <div>
+      {props.optionText}
+    </div>
+  )
 }
 
 class AddOption extends React.Component {
@@ -136,4 +128,13 @@ class AddOption extends React.Component {
   }
 }
 
-ReactDOM.render(<IndecisionApp />, document.getElementById('app'))
+const User = (props) => {
+  return (
+    <div>
+      <p>Name: {props.name}</p>
+      <p>Age: {props.age}</p>
+    </div>
+  );
+};
+
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
