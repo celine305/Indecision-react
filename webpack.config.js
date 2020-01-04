f357ce4d9893c9c5413005060cea8d7f9be75c3f
celine305 <celine.abessolo@icloud.com>
2020-01-04 13:03:33 +0000

    swith over to webpack devServer

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   entry: './src/app.js',
   output: {
-    path: path.join(__dirname, 'public'), //absolute path
+    path: path.join(__dirname, 'public'), //has to be absolute path
     filename: 'bundle.js'
   },
   module: {
@@ -13,5 +13,8 @@ module.exports = {
       exclude: /node_modules/  //folder to exclude
     }]
   },
-  devTool: 'cheap-module-eval-source-map',
+  devtool: 'cheap-module-eval-source-map',
+  devServer: {
+    contentBase: path.join(__dirname, 'public') //absolute path to those public assets (the public folder)
+  }
 };
